d9e010e771b1209bf03222a2c745b04c2971975d
kyshin <kyshin1993@gmail.com>
2024-06-19 16:47:47 +0200

    Authentication via JWT has been implemented JwtAuthenticationController - endpoint which returns auth jwt token if authentication is successful JwtTokenService - service for serving and building jwt JwtTokenRequest and JwtTokenResponse - transport dtos

---
+++ ArmorDriveJava/src/main/java/ua/com/marketplace/armordrive/jwt/JwtTokenRequest.java
@@ -0,0 +1,4 @@
+package ua.com.marketplace.armordrive.jwt;
+
+public record JwtTokenRequest(String username, String password) {
+}
---
+++ ArmorDriveJava/src/main/java/ua/com/marketplace/armordrive/jwt/JwtTokenResponse.java
@@ -0,0 +1,4 @@
+package ua.com.marketplace.armordrive.jwt;
+
+public record JwtTokenResponse(String token) {
+}
